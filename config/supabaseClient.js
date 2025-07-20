a57118671aed1df78484a14d9ce956dc6f5059e2
Arslan Aslam <itsarslanaslam@gmail.com>
2025-07-20 21:35:52 +0500

    Add files via upload

---
+++ config/supabaseClient.js
@@ -0,0 +1,13 @@
+const { createClient } = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
